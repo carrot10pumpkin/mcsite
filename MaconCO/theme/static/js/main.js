8ac7576d319b1782e3ba70f7b88ebd2463db99db
carrot10pumpkin <carrot10pumpkin@hotmail.com>
2023-12-07 15:24:20 -0500

    Corrected latest project controls

--- MaconCO/theme/static/js/main.js
+++ MaconCO/theme/static/js/main.js
@@ -1,5 +1,6 @@
 var $content = $('#imgsList'); // Cache your selectors!
-$(".cont").hover( function loop() {
+
+$(".slide-control.left").hover( function loop() {
     $content.stop().animate({  marginLeft: '-=1600' }, 5000, 'linear', function(){
         $content.css({ marginLeft : "0px" });
         loop();
@@ -7,6 +8,30 @@ $(".cont").hover( function loop() {
 }, function stop() {
     $content.stop();
 });
+
+$(".slide-control.right").hover( function loop() {
+    $content.stop().animate({  marginLeft: '+=1600' }, 5000, 'linear', function(){
+        $content.css({ marginLeft : -$content.outerWidth() });
+        loop();
+    });
+}, function stop() {
+    $content.stop();
+});
+
+
+
+
+
+
+// var $content = $('#imgsList'); // Cache your selectors!
+// $(".cont").hover( function loop() {
+//     $content.stop().animate({  marginLeft: '-=1600' }, 5000, 'linear', function(){
+//         $content.css({ marginLeft : "0px" });
+//         loop();
+//     });
+// }, function stop() {
+//     $content.stop();
+// });
 
 
 // var $content = $('#imgsList');
