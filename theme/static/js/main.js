299b434876a452d4c7478c983b08da2ecc4a3e09
carrot10pumpkin <carrot10pumpkin@hotmail.com>
2023-12-11 10:16:00 -0500

    Correction to slider test

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -1,5 +1,6 @@
 var $content = $('#imgsList'); // Cache your selectors!
-$(".cont").hover( function loop() {
+
+$(".slide-control.left").hover( function loop() {
     $content.stop().animate({  marginLeft: '-=1600' }, 5000, 'linear', function(){
         $content.css({ marginLeft : "0px" });
         loop();
@@ -8,6 +9,14 @@ $(".cont").hover( function loop() {
     $content.stop();
 });
 
+$(".slide-control.right").hover( function loop() {
+    $content.stop().animate({  marginLeft: '+=1600' }, 5000, 'linear', function(){
+        $content.css({ marginLeft : -$content.outerWidth() });
+        loop();
+    });
+}, function stop() {
+    $content.stop();
+});
 
 // var $content = $('#imgsList');
 
